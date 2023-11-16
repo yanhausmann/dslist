8882cb9f480ff89afdb189cafd0b74668f61e547
hausmen <yanfhausmann@gmail.com>
2023-11-16 16:07:38 -0300

    SQL Query + Projections

--- src/main/java/com/yanhausmann/dslist/services/GameService.java
+++ src/main/java/com/yanhausmann/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.yanhausmann.dslist.services;
 import com.yanhausmann.dslist.dto.GameDTO;
 import com.yanhausmann.dslist.dto.GameMinDTO;
 import com.yanhausmann.dslist.entities.Game;
+import com.yanhausmann.dslist.projections.GameMinProjection;
 import com.yanhausmann.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 
